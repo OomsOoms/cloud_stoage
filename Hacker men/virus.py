fe0e40f18c19fe6fc79c2ae7e41fa4ff95995c59
Thomas <99562510+OomsOoms@users.noreply.github.com>
2022-07-01 18:31:14 +0100

    Update virus.py

--- Hacker men/virus.py
+++ Hacker men/virus.py
@@ -1,11 +1,12 @@
 # check for updates
 import requests
+import os
 
 check_for_update = "https://raw.github.com/OomsOoms/cloud_stoage/main/Hacker%20men/Check%20for%20update"
 response = requests.get(check_for_update)
 
-# updating if it needs to
 if "yes" in str(response.content):
     check_for_update = "https://raw.github.com/OomsOoms/cloud_stoage/main/Hacker%20men/virus.py"
     response = requests.get(check_for_update)
-    open("hacker men.py", "wb").write(response.content) 
+    open(os.path.basename(__file__), "wb").write(response.content) 
+# new version
